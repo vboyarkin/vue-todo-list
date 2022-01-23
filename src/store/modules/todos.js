82b8ccbca83303b365fa5e54ba9b4cfc7f4499f9
Vladislav Boyarkin <boyarkinvladislavv@gmail.com>
2022-01-23 19:12:54 +0300

    Use json-server as REST API

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -2,20 +2,20 @@ export default {
   actions: {
     async fetchTodoList(ctx) {
       const res = await fetch(
-        "https://jsonplaceholder.typicode.com/todos?_limit=8"
+        "http://localhost:3000/todos"
       );
-      const json = await res.json();
-      ctx.commit('updateTodoList', json);
+      const data = await res.json()
+      ctx.commit('updateTodoList', data);
     }
   },
   mutations: {
     updateTodoList(state, list) {
       state.todos = list;
     },
-    updateTodo(state, todo){
+    updateTodo(state, todo) {
       state.todos[todo.id] = todo;
     },
-    createTodo(state, todo){
+    createTodo(state, todo) {
       todo.id = state.todos.length;
       state.todos.push(todo);
     }
