23821994bbd7d7ab517195ba1035d70b2eaad45b
Vladislav Boyarkin <boyarkinvladislavv@gmail.com>
2022-01-27 13:31:58 +0300

    Address minor codestyle issues

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './App.vue'
-import router from './router'
-import store from './store';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
-Vue.use(VueRouter)
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
 new Vue({
   render: h => h(App),
   router,
   store,
-}).$mount('#app')
+}).$mount("#app");
