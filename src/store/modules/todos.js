7f5b7b4769a183e630351ebc12e19c7df74ed494
Vladislav Boyarkin <boyarkinvladislavv@gmail.com>
2022-01-27 13:25:26 +0300

    Use unique id, optimize state handling in todos.js

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,61 +1,58 @@
 export default {
-  actions: {
-    fetchTodoList({ commit, dispatch }) {
-      let todos = localStorage.getItem("todos");
-      if (!todos) {
-        todos = [];
-        dispatch('saveToStorage', []);
-      } else
-        todos = JSON.parse(todos)
-      commit('updateTodoList', todos);
-    },
-    saveToStorage({ state }, todos = state.todos) {
-      localStorage.setItem("todos", JSON.stringify(todos));
-    },
-    updateTodo({ commit, dispatch }, todo) {
-      commit('updateTodo', todo)
-      dispatch('saveToStorage');
-    },
-    createTodo({ commit, dispatch }, todo) {
-      commit('createTodo', todo)
-      dispatch('saveToStorage');
-    },
-    deleteTodo({ commit, dispatch }, id) {
-      commit('deleteTodo', id)
-      dispatch('saveToStorage');
-    }
-  },
-  mutations: {
-    updateTodoList(state, list) {
-      state.todos = list;
-    },
-    updateTodo(state, todo) {
-      state.todos[todo.id - 1] = todo;
-    },
-    createTodo(state, todo) {
-      todo.id = state.todos.length + 1;
-      state.todos.push(todo);
-    },
-    deleteTodo(state, id) {
-      if (id - 1 <= state.todos.length)
-        state.todos.splice(id - 1, 1)
-    }
-  },
   state: {
-    todos: []
+    todos: [],
   },
   getters: {
     allTodos(state) {
       return state.todos;
     },
-    getTodo:
-      (state) => (id) => {
-        if (id - 1 >= state.todos.length) return null;
-
-        console.log('id :>> ', id);
-
-        console.log("getTodo()");
-        return state.todos[id - 1];
-      },
+    getTodo: state => id => {
+      return state.todos.find(todo => todo.id === id);
+    },
   },
-}
+  actions: {
+    fetchTodoList({ commit }) {
+      let todos = localStorage.getItem("todos");
+      if (todos) {
+        try {
+          const parsedTodos = JSON.parse(todos);
+          commit("updateTodoList", parsedTodos);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    },
+    saveToStorage({ state }, todos = state.todos) {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    },
+    updateTodo({ commit, dispatch }, todo) {
+      commit("updateTodo", todo);
+      dispatch("saveToStorage");
+    },
+    createTodo({ commit, dispatch }, todo) {
+      commit("createTodo", todo);
+      dispatch("saveToStorage");
+    },
+    deleteTodo({ commit, dispatch }, id) {
+      commit("deleteTodo", id);
+      dispatch("saveToStorage");
+    },
+  },
+  mutations: {
+    updateTodoList(state, list) {
+      state.todos = list;
+    },
+    updateTodo(state, newTodo) {
+      const todoi = state.todos.findIndex(todo => todo.id === newTodo.id);
+      state.todos.splice(todoi, 1, newTodo);
+    },
+    createTodo(state, todo) {
+      todo.id = todo.createdAt + todo.title;
+      state.todos.push(todo);
+    },
+    deleteTodo(state, id) {
+      const todoi = state.todos.findIndex(todo => todo.id === id);
+      state.todos.splice(todoi, 1);
+    },
+  },
+};
